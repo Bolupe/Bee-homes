56501e99840b0deb626562d42c4a1ee049b8e8df
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-10-14 14:41:15 +0100

    created  routes and header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Home from './pages/Home'; 
+import Profile from './pages/Profile'; 
+import Header from './components/Header';
+
 
 export default function App() {
   return (
-    <div>
-          <h1 className="text-red-200">App</h1>
-    </div>
-  )
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/SignIn' element={<SignIn />} />
+        <Route path='/SignUp' element={<SignUp />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
- 
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>About</div>
+  )
+}
+
+// switch this to the home page, a landing page
+// assigned to Micheal 
