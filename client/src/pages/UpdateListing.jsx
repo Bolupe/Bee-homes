aed22a0c681ed047b533ac021b5bb61c9df9de11
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-11-04 00:11:02 +0100

    completed the listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -297,8 +297,10 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
-              </div>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}            
+                </div>
             </div>
             {formData.offer && (
               <div className='flex items-center gap-2'>
@@ -314,8 +316,10 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>($ / month)</span>
-                </div>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}                
+                  </div>
               </div>
             )}
           </div>
